17e22b2343459298a39dec181697cec2da848649
1VishalSD <ilearnvk@gmail.com>
2024-12-28 15:13:39 +0530

    start to building the signup page for the my lms system

---
+++ clients/src/constants/FormField.jsx
@@ -0,0 +1,23 @@
+const signinField = [
+  {
+    labelText: "Email addrress",
+    labelFor: " email-address",
+    id: " email-address",
+    name: "email",
+    type: "email",
+
+    autoComplete: " email",
+    isRequired: true,
+    palceholder: " Email address",
+  },
+  {
+    labelText: "Password",
+    labelFor: "password",
+    id: "password",
+    name: "password",
+    type: "password",
+    autoComplete: "password",
+    isRequired: true,
+    palceholder: " Password",
+  },
+];
