2d9247b186602330a0ba95ea88f0133cbe0b005f
1VishalSD <ilearnvk@gmail.com>
2025-01-13 09:30:48 +0530

    start to building

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
-import express from "express";
-const app = express();
-const port = 3000;
-app.get("/", (req, res) => {
-  res.send(
-    "Hello I am buildig the app on the typescript + express + nodejs backend!"
-  );
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { dot } from "node:test/reporters";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req: Request, res: Response) => {
+  res.send("TypeScript + Express");
 });
 
 app.listen(port, () => {
-  console.log(`Server is runing on the port ${port}`);
+  console.log(`Your Server is running on the Port : ${port}`);
 });
