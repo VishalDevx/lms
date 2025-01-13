ddfdf1e6cd7eaf8cc5093fda4fc0c9e8a65bc61d
1VishalSD <ilearnvk@gmail.com>
2025-01-14 05:28:40 +0530

    rewrite the authControlles

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -3,47 +3,39 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import prisma from "../config/db"; // Prisma client instance
 import { Role } from "@prisma/client";
+import { error } from "console";
 
 const secret: string = process.env.JWT_SECRET || "";
-
 export const login = async (req: Request, res: Response): Promise<void> => {
   const { email, password } = req.body;
 
   try {
-    const admin = await prisma.admin.findUnique({
-      where: { email },
-    });
-
+    const admin = await prisma.admin.findUnique({ where: { email } });
     if (!admin) {
-      res.status(401).json({ msg: "User not found!" });
+      res.status(401).json({ msg: "User is not Found!" });
       return;
     }
-
     const isMatch = await bcrypt.compare(password, admin.password);
-
     if (!isMatch) {
-      res.status(401).json({ msg: "Invalid Credentials!" });
-      return;
+      res.status(401).json({
+        msg: " Invalid Credential",
+      });
     }
-
     if (!secret) {
-      throw new Error("JWT_SECRET is not defined");
+      throw new Error("Jwt secret is not defined");
     }
-
     const token = jwt.sign({ id: admin.id, Role: admin.role }, secret, {
       expiresIn: "1h",
     });
-
     res.status(200).json({ token });
   } catch (error) {
-    console.error("Error during login:", error);
+    console.error("Internel Server Error", error);
     res.status(500).json({
-      msg: "Internal Server error",
+      msg: " Error During login ",
       error,
     });
   }
 };
-
 export const verifyToken = async (
   req: Request,
   res: Response
