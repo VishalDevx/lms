5e33f5bc3f4aff70fb29852e6378b80d08b1a425
1VishalSD <ilearnvk@gmail.com>
2025-01-14 12:49:25 +0530

    writing the auth

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -2,7 +2,7 @@ import express, { Router } from "express"; // Import express to use Router
 import { login, verifyToken } from "../controllers/authControllers";
 
 const router: Router = express.Router(); // Use the imported express to call Router
-authRoute.get("api/login", login);
-authRoute.post("api/verify", verifyToken);
+router.post("/login", login);
+router.post("/verify", verifyToken);
 
 export default router;
