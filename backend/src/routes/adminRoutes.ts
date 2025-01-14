103ba796be7538ed71100ccfc47584d266c50117
1VishalSD <ilearnvk@gmail.com>
2025-01-14 05:35:15 +0530

    write down the gitignore file "

--- backend/src/routes/adminRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { login, verifyToken } from "../controllers/authControllers";
-
-const router = express.Router();
-router.get("/login", login);
-router.post("/verify", verifyToken);
-export default router;
