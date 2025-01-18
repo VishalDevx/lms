dcf523ab18bc2012de380728aa0ff6fee839ad04
1VishalSD <ilearnvk@gmail.com>
2025-01-18 20:03:56 +0530

    error

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -5,9 +5,16 @@ import {
   uniqueStudents,
   students,
 } from "../controllers/adminControllers/studentControllers";
+import { feeValidation } from "../middlewares/feesValidation";
+import {
+  createStudentFee,
+  studentFee,
+} from "../controllers/adminControllers/feesControllers";
 const router: Router = express.Router();
 
 router.post("/createstudent", studentValidation, createStudent);
 router.get("/uniqueStudents/:rollNumber", uniqueStudents);
 router.get("/students", students);
+router.post("/create_student_fee", feeValidation, createStudentFee);
+router.get("/student_fee/:id", studentFee);
 export default router;
