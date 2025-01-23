37191bae598dc2dd510f1d1c16b07cdeb1e545e4
1VishalSD <ilearnvk@gmail.com>
2025-01-23 21:21:49 +0530

    prepare for the exam so that why not avail to do the code

--- backend/src/controllers/adminControllers/studentsControllers.ts
+++ backend/src/controllers/adminControllers/studentsControllers.ts
@@ -1,12 +1,38 @@
 import { Request, Response } from "express";
 
 import prisma from "../../config/db";
+import { Gender } from "@prisma/client";
 
 export const studentSchema = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
-    const {name ,fatherName ,motherName,gender,bloodGroup,class,mobileNumber,address,profilePic,rollNumber,createdAt,updatedAt}= req.body
+    const {
+      name,
+      fatherName,
+      motherName,
+      gender,
+      bloodGroup,
+      grade,
+      mobileNumber,
+      address,
+      profilePic,
+      rollNumber,
+      createdAt,
+      updatedAt,
+    } = req.body;
+
+    const addStudent = await prisma.student.create({
+      data: {
+        name,
+        fatherName,
+        motherName,
+        gender,
+        bloodGroup,
+        grade,
+        mobileNumber,
+      },
+    });
   } catch (error) {}
 };
