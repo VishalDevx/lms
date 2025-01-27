91476de15b507b6b2a4a877e3351975b8c6e2e78
1VishalSD <ilearnvk@gmail.com>
2025-01-27 16:12:41 +0530

    good is going

--- backend/src/routes/admin/admin.route.ts
+++ backend/src/routes/admin/admin.route.ts
@@ -1,5 +1,4 @@
 import express, { Router } from "express";
-import { studentSchema } from "../../middlewares/studentValidation";
 
 const adminRoutes = Router();
 adminRoutes.post("/addStudent", adminRoutes);
