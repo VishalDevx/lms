e35aa1e3a05433c1b7ce33c8dc2d43eecf96a4f3
1VishalSD <ilearnvk@gmail.com>
2025-01-29 12:48:41 +0530

    start again to write the logic for the adding the students

--- backend/src/routes/admin/admin.route.ts
+++
@@ -1,6 +0,0 @@
-import express, { Router } from "express";
-import { add_student } from "../../controllers/adminControllers/students.controllers";
-const adminRoutes = Router();
-adminRoutes.post("/add_student", add_student);
-
-export default adminRoutes;
