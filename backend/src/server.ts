5c470fa0e70cc09b9860c13a3027a3c2be84d1b9
1VishalSD <ilearnvk@gmail.com>
2025-03-24 17:47:18 +0530

    setting the cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import adminRoutes from "./routes/admin/admin.route";
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/v1/admin/", adminRoutes);
