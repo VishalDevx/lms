981d567fe5a79bba89dab8a9c9f0079728afe655
1VishalSD <ilearnvk@gmail.com>
2025-05-22 16:41:06 +0530

    start to building the frontend for the admin login

--- frontend/src/components/addStudentForm.tsx
+++ frontend/src/components/addStudentForm.tsx
@@ -2,14 +2,7 @@ import React, { useState } from "react";
 import useStudentForm from "../hooks/useStudentForm";
 import { studentSchema } from "@vishaldevsx/lms-common";
 
-// Define the type for your form data
-interface StudentFormData {
-  // Add your form fields here, for example:
-  name?: string;
-  age?: number;
-  // etc.
-}
-
+interface
 const StudentForm: React.FC = () => {
   const [formData, setFormData] = useState<StudentFormData>({});
 
